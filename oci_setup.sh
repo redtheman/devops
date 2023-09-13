5669d6721034ac65d86f3c2d46210e6c743c49a6
redtheman <redtheman@users.noreply.github.com>
2023-09-13 12:58:37 +0300

    Create oci_setup.sh

---
+++ oci_setup.sh
@@ -0,0 +1,6 @@
+# This is a tutorial on setting up OCI for php, sql developer and related tools on a linux box. For the purpose of this tutorial,
+# this was run on an ec2 instance running almalinux- 8.8
+
+# First is to download the basic, sql plus, tools  package 
+wget https://download.oracle.com/otn_software/linux/instantclient/1920000/oracle-instantclient19.20-basic-19.20.0.0.0-1.x86_64.rpm
+wget https://download.oracle.com/otn_software/linux/instantclient/1920000/oracle-instantclient19.20-sqlplus-19.20.0.0.0-1.x86_64.rpm
